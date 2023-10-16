ab66deb9facb31226a6c7b716fb4d53cf6594a50
vaibhav <vp84858402@gmail.com>
2023-10-16 17:42:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -14,14 +14,23 @@ import CreateAcc from './components/CreateAcc';
 
 
 function App() {
+
+  const myStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/back4.jpg)`,
+    height: '120vh',
+    marginTop: '-70px',
+    fontSize: '50px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  };
   return (
     <>
-    <Navbar/>
+    <Navbar st={myStyle}/>
 
    
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home st={myStyle} />} />
         <Route path="/bank" element={
           <Bank/>
         } />
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export default function Home(props) {
   const myStyle = {
-    backgroundImage: `url(${process.env.PUBLIC_URL}/back.jpg)`,
+    backgroundImage: `url(${process.env.PUBLIC_URL}/back4.jpg)`,
     height: '120vh',
     marginTop: '-70px',
     fontSize: '50px',
@@ -12,23 +12,39 @@ export default function Home(props) {
   };
 
   const divstyle = {
-    paddingTop: '550px',
-    paddingLeft: '600px',
+    paddingTop: '10%',
+    align: 'center',
   };
 
   const btnstyle = {
     width: '200px',
   };
+  const bankimgStyle = {
 
+    width: '200px',
+    height: '200px',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    paddingTop:'100px',
+    marginBottom:'100px',
+    
+    
+  };
   return (
     <div className="container-fluid" style={myStyle}>
+      
       <div className="container" style={divstyle}>
+      <img src={`${process.env.PUBLIC_URL}/bnk2.png`} style={bankimgStyle} alt='bank'/>
+        <div className="container text-center">
+
         <h1>Welcome to Bank of Interns</h1>
         <Link to="/bank">
           <button type="button" style={btnstyle} className="btn btn-light">
             Get Started
           </button>
         </Link>
+        </div>
       </div>
     </div>
   );
